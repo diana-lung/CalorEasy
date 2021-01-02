41fd5d2a86f145f60be5d7121ecefee30e1c3a5e
Diana Lung <diana.lung@3pillarglobal.com>
2021-01-02 02:07:19 +0200

    [CEA-02] Create Authentication screens
    - created necessary UI elements + styling
    - login + signup screens added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,129 @@
-import {Text} from "react-native";
-import React from "react";
+import React, {useContext, useState} from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    Platform,
+    StyleSheet,
+} from 'react-native';
+import FormButton from '../components/FormButton';
+import FormInput from "../components/FormInput";
+import {ScrollView} from "react-native-gesture-handler";
+import SocialButton from "../components/SocialButton";
+import {windowHeight} from "../utils/Dimensions";
+// import {AuthContext} from '../navigation/AuthProvider';
 
-const LoginScreen = (navigation) => {
+const LoginScreen = ({navigation}) => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    // const {login, googleLogin, fbLogin} = useContext(AuthContext);
 
     return (
-        <Text>Login Screen</Text>
+        <ScrollView contentContainerStyle={styles.container}>
+            <Image
+                source={require('../assets/images/icon.png')}
+                style={styles.logo}
+            />
+            <Text style={styles.text}>CalorEasy App</Text>
+
+            <FormInput
+                labelValue={email}
+                // onChangeText={(userEmail) => setEmail(userEmail)}
+                placeholderText="Email"
+                iconType="user"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+
+            <FormInput
+                labelValue={password}
+                // onChangeText={(userPassword) => setPassword(userPassword)}
+                placeholderText="Password"
+                iconType="lock"
+                secureTextEntry={true}
+            />
+
+            <FormButton
+                buttonTitle="Sign In"
+                onPress={() => {
+                }}
+                // onPress={() => login(email, password)}
+            />
+
+            <TouchableOpacity
+                style={styles.forgotButton}
+                onPress={() => navigation.navigate('Signup')}>
+                <Text style={styles.navButtonText}>
+                    Don't have an acount?  <Text style={styles.navButtonTextBold}>Create here</Text>
+                </Text>
+            </TouchableOpacity>
+
+            {Platform.OS === 'android' ? (
+                <View>
+                    <SocialButton
+                        buttonTitle="Sign In with Facebook"
+                        btnType="facebook"
+                        color="#4867aa"
+                        borderColor="#4867aa"
+                        // onPress={() => fbLogin()}
+                    />
+
+                    <SocialButton
+                        buttonTitle="Sign In with Google"
+                        btnType="google"
+                        color="#de4d41"
+                        borderColor="#de4d41"
+                        // onPress={() => googleLogin()}
+                    />
+                </View>
+            ) : null}
+
+
+        </ScrollView>
     );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: 20,
+        paddingTop: 50,
+        backgroundColor: '#fFF',
+        height: windowHeight
+    },
+    logo: {
+        height: 150,
+        width: 150,
+        resizeMode: 'cover',
+    },
+    text: {
+        fontFamily: 'Kufam-SemiBoldItalic',
+        fontSize: 28,
+        marginBottom: 10,
+        color: '#3f3d56',
+    },
+    navButton: {
+        marginTop: 15,
+    },
+    forgotButton: {
+        marginVertical: 35,
+    },
+    navButtonText: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#888',
+        fontFamily: 'Lato-Regular',
+    },
+    navButtonTextBold: {
+        fontSize: 16,
+        color: '#696969',
+        fontFamily: 'Lato-Bold',
+    }
+});
+
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,11 +1,120 @@
-import {Text} from "react-native-web";
-import React from "react";
+import React, {useContext, useState} from 'react';
+import {View, Text, TouchableOpacity, Platform, StyleSheet, Image} from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import SocialButton from '../components/SocialButton';
+import {ScrollView} from "react-native-gesture-handler";
+import {windowHeight} from "../utils/Dimensions";
+// import {AuthContext} from '../navigation/AuthProvider';
 
-const SignupScreen = (navigation) => {
+const SignupScreen = ({navigation}) => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+
+    // const {register} = useContext(AuthContext);
+
 
     return (
-        <Text>Signup Screen</Text>
+        <ScrollView contentContainerStyle={styles.container}>
+            <Text style={styles.text}>Create an account</Text>
+
+            <FormInput
+                labelValue={email}
+                // onChangeText={(userEmail) => setEmail(userEmail)}
+                placeholderText="Email"
+                iconType="user"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+
+            <FormInput
+                labelValue={password}
+                // onChangeText={(userPassword) => setPassword(userPassword)}
+                placeholderText="Password"
+                iconType="lock"
+                secureTextEntry={true}
+            />
+
+            <FormInput
+                labelValue={confirmPassword}
+                // onChangeText={(userPassword) => setPassword(userPassword)}
+                placeholderText="Confirm Password"
+                iconType="lock"
+                secureTextEntry={true}
+            />
+
+            <FormButton
+                buttonTitle="Sign Up"
+                // onPress={() => register(email, password)}
+            />
+
+            <TouchableOpacity
+                style={styles.navButton}
+                onPress={() => navigation.navigate('Login')}>
+                <Text style={styles.navButtonText}>Have an account? <Text style={styles.navButtonTextBold}>Sign In</Text></Text>
+            </TouchableOpacity>
+
+            {Platform.OS === 'android' ? (
+                <View>
+                    <SocialButton
+                        buttonTitle="Sign Up with Facebook"
+                        btnType="facebook"
+                        color="#4867aa"
+                        borderColor="#4867aa"
+                        // onPress={() => {}}
+                    />
+
+                    <SocialButton
+                        buttonTitle="Sign Up with Google"
+                        btnType="google"
+                        color="#de4d41"
+                        borderColor="#de4d41"
+                        // onPress={() => {}}
+                    />
+                </View>
+            ) : null}
+        </ScrollView>
+
     );
 };
 
 export default SignupScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#fFF',
+        height: windowHeight
+    },
+    logo: {
+        height: 140,
+        width: 140,
+        resizeMode: 'cover',
+    },
+    text: {
+        fontFamily: 'Kufam-SemiBoldItalic',
+        fontSize: 28,
+        marginTop: 45,
+        marginBottom: 35,
+        color: '#051d5f',
+    },
+    navButton: {
+        marginTop: 30,
+        marginBottom: 40
+    },
+    navButtonText: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#888',
+        fontFamily: 'Lato-Regular',
+    },
+    navButtonTextBold: {
+        fontSize: 16,
+        color: '#777',
+        fontFamily: 'Lato-Bold',
+    },
+});
